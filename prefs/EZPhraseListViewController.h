b66c0bf0ae1627f44026084840f7f08663a1f850
imod-commission <107318820+imod-commission@users.noreply.github.com>
2026-09-23 15:51:36 +0900

    Add whole-word parameters to phrase list APIs.

--- prefs/EZPhraseListViewController.h
+++ prefs/EZPhraseListViewController.h
@@ -1,16 +1,11 @@
-// EZPhraseListViewController.h
-
 #import <Preferences/PSViewController.h>
 
 @interface EZPhraseListViewController : PSViewController <UITableViewDataSource, UITableViewDelegate> {
-	NSMutableDictionary *_settings;
-	NSMutableDictionary *_strings;
-	NSMutableDictionary<NSString *, NSMutableArray *> *_sortedStrings;
+    NSMutableDictionary *_settings;
+    NSMutableDictionary *_strings;
+    NSMutableDictionary<NSString *, NSMutableArray *> *_sortedStrings;
 }
-
 @property (nonatomic, retain) UITableView *tableView;
-
-- (void)addPhrase:(NSString *)addPhrase replacement:(NSString *)replacement caseSensitive:(BOOL)caseSensitive;
-- (void)editPhrase:(NSString *)phrase newPhrase:(NSString *)newPhrase replacement:(NSString *)replacement caseSensitive:(BOOL)caseSensitive;
-
+- (void)addPhrase:(NSString *)phrase replacement:(NSString *)replacement caseSensitive:(BOOL)caseSensitive wholeWord:(BOOL)wholeWord;
+- (void)editPhrase:(NSString *)phrase newPhrase:(NSString *)newPhrase replacement:(NSString *)replacement caseSensitive:(BOOL)caseSensitive wholeWord:(BOOL)wholeWord;
 @end
